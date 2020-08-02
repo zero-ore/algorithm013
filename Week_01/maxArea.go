eee6b5f5282b3d2dee80af8a619795b07f6c2ab8
xugf <2297514194@qq.com>
2020-08-02 21:54:37 +0800

    maxArea

--- Week_01/maxArea.go
+++ Week_01/maxArea.go
@@ -1,5 +1,13 @@
 package main
 
+//盛最多的水 https://leetcode-cn.com/problems/container-with-most-water/submissions/
+
+/**
+ * 解法2
+ * 1、i指向第一个元素，j指向第二个元素
+ * 2、比较i、j的值，取最小值
+ * 3、计算两根柱子围成的长方形面积，并取最大值
+ */
 func maxArea2(height []int) int {
 	marea := 0
 	i := 0
@@ -7,11 +15,11 @@ func maxArea2(height []int) int {
 	minHeight := 0
 	for i < j {
 		if height[i] < height[j] {
-			i = i + 1
 			minHeight = height[i]
+			i = i + 1
 		} else {
-			j = j - 1
 			minHeight = height[j]
+			j = j - 1
 		}
 		area := (j - i + 1) * minHeight
 		marea = Max(area, marea)
@@ -19,6 +27,9 @@ func maxArea2(height []int) int {
 	return marea
 }
 
+/**
+ * 解法1：嵌套循环，数组下标i与每个元素a组成（x，y）坐标，分别穷举计算面积
+ */
 func maxArea(height []int) int {
 	if len(height) < 2 {
 		return 0
